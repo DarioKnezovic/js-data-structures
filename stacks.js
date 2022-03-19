a1977337ec1d36a3223a7a25d46ba8a922af8a97
DarioKnezovic <dknezovic997@gmail.com>
2022-03-19 18:34:42 +0100

    Stacks introduced.

---
+++ stacks.js
@@ -0,0 +1,63 @@
+class Node {
+    constructor(value){
+        this.value = value;
+        this.next = null;
+    }
+}
+
+class Stack {
+    constructor(){
+        this.top = null;
+        this.bottom = null;
+        this.length = 0;
+    }
+    loop() {
+        let pointer = this.top;
+
+        while (pointer) {
+            console.log(pointer.value);
+            pointer = pointer.next;
+        }
+    }
+    peek() {
+        return this.top;
+    }
+    push(value){
+        const newNode = new Node(value);
+
+        if (this.top && this.bottom) {
+            newNode.next = this.top;
+            this.top = newNode;
+        }else {
+            this.top = newNode;
+            this.bottom = newNode;
+        }
+
+        this.length++;
+    }
+    pop(){
+        if (!this.top) {
+            return;
+        }
+
+        if (this.top === this.bottom) {
+            this.bottom = null;
+        }
+
+        this.top = this.top.next;
+        this.length--;
+    }
+}
+
+const myStack = new Stack();
+myStack.push("Google");
+myStack.push("Udemy");
+myStack.push("Discord");
+myStack.loop();
+console.log("-------");
+myStack.pop();
+myStack.loop();
+
+//Discord
+//Udemy
+//google
